af07b1375fa4db6ecfd18938e55249bf1270ed4e
Ctibor Kovalčík <ckkoval33@gmail.com>
2021-03-03 00:04:28 +0100

    IDK

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <string.h>
 #include <time.h>
 
+#define VELKOST 1000
 
-char region[100000];
+
+int region[VELKOST];
 typedef struct block
 {
-    struct block* prev;
-    struct block *next;
-    int free;
     int size;
+    char free;
+    struct block *next;
+
+
 
 }BLOCK;
 
@@ -26,87 +29,45 @@ BLOCK *list= region;
 
 void* memory_alloc(unsigned int size) {
 
-
-    //printf("memory alloc");
     if( size <= 0 ){
         return NULL;
     }
 
+    int memory_used=0;
+    while(list->free != 1){
+        list = list->next;
+        memory_used = memory_used + list->size;
+    }
 
-
-    for (int i= 0 ; i < 100 ; i++){
-	    if (region[i] == 2){
-	        char *ptr_alloc;
-	        ptr_alloc = &region[i];
-            //printf("pointer validptr ukazuje na adresu : %p\n", ptr_alloc);
-	        region[i] = size;
-	        region[i+1] =0;
-            region[i+2] =0;
-            region[i+3] =0;
-            for (int j = 0 ; j < size-4 ; j++ ){
-                region[i+4]=-1;
-                i++;
-
-            }
-
-            list->prev = NULL;
-            list->next = ptr_alloc + size;
-	        list->free = 0;
-	        list->size = size;
-	        list = list->prev;
-	        list = (list->next);
-
-
-            return ptr_alloc;
-
-	    }
-	}
-
+    list->size = size;
+    list->free = 0;
+    list->next = list+size;
+    list = list->next;
+    list->size = VELKOST - memory_used - size;
+    list->free = 1;
 
 
 }
+
 int memory_free(void* valid_ptr) {
-    valid_ptr = 5;
-    //printf("Memory free");
     return 1;
 
 }
 int memory_check(void* ptr) {
-   // printf("Memory check");
     return 1;
 
 }
+
+
+
 void memory_init(void* ptr, unsigned int size) {
 
-	//printf("vstupil si do memory init\n");
-	//printf("pointer ptr ukazuje na adresu : %p\n", ptr);
-
-	list->next = ptr+4;
-	list->size = size;
-	list->free = 1;
-	list->prev = NULL;
-
-	char* a;
-    a = ptr;
-    //printf("pointer a ukazuje na adresu : %p\n", a);
-
-    *a = size;
-    *(a+1) = 0;
-    *(a+2) = 0;
-    *(a+3) = 0;
-    a = a+4;
+    list->size = size;
+    list->free = 1;
+    list->next = ptr + size;
 
 
-	for(int i = 3 ; i < size ;i++){
-
-        *a = 2;
-        a++;
-	}
-    //printf("pointer a ukazuje na adresu : %p\n", a);
-	*a = "a";
-	*a = 5;
 }
-
 
 int main(){
     memory_init(region, 100);   // Initialization of my memory of 50 bytes
@@ -116,5 +77,4 @@ int main(){
     memset(region, 0, 10);
     return 0;
 }
-
 
